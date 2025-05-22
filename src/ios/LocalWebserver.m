9a778231e6348a91baef7698bdfb781255206d6b
Bence Kovacs <kov.benc@gmail.com>
2025-05-22 14:05:43 +0200

    Update LocalWebserver.m

--- src/ios/LocalWebserver.m
+++ src/ios/LocalWebserver.m
@@ -2,6 +2,14 @@
 #import <ifaddrs.h>
 #import <arpa/inet.h>
 
+// Helper wrapper for request state
+@interface RequestWrapper : NSObject
+@property (nonatomic, strong) dispatch_semaphore_t semaphore;
+@property (nonatomic, strong) NSDictionary* response;
+@end
+
+@implementation RequestWrapper
+@end
 
 @implementation LocalWebserver
 
@@ -12,64 +20,128 @@
 - (void)start:(CDVInvokedUrlCommand*)command {
     NSInteger port = [command.arguments[0] integerValue];
     webServer = [[GCDWebServer alloc] init];
-    __weak __typeof(self) weakSelf = self;
+    __weak __typeof__(self) weakSelf = self;
 
-    [webServer addDefaultHandlerForMethod:@"GET"
-                              requestClass:[GCDWebServerDataRequest class]
-                              processBlock:^GCDWebServerResponse*(GCDWebServerRequest* request) {
-        NSString* reqId = [[NSUUID UUID] UUIDString];
-        NSMutableDictionary* wrapper = [NSMutableDictionary dictionary];
-        wrapper[@"request"] = request;
-        wrapper[@"semaphore"] = dispatch_semaphore_create(0);
-        pendingRequests[reqId] = wrapper;
-        // Notify JS
-        NSDictionary* jsReq = @{
-            @"requestId": reqId,
-            @"method": request.method,
-            @"path": request.path,
-            @"query": request.query
-        };
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsReq];
-        [pluginResult setKeepCallbackAsBool:YES];
-        [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:requestCallbackId];
-        // Wait for JS response (timeout 30s)
-        dispatch_semaphore_t sem = wrapper[@"semaphore"];
-        long result = dispatch_semaphore_wait(sem, dispatch_time(DISPATCH_TIME_NOW, 30 * NSEC_PER_SEC));
-        if (result != 0) {
-            GCDWebServerDataResponse* timeoutResp = [GCDWebServerDataResponse responseWithText:@"Timeout waiting for response"];
-            timeoutResp.statusCode = 500;
-            return timeoutResp;
-        }
-        NSDictionary* resp = wrapper[@"response"];
-        NSInteger status = [resp[@"status"] integerValue];
-        NSDictionary* headers = resp[@"headers"];
-        NSString* body = resp[@"body"];
-        GCDWebServerDataResponse* responseObj = [GCDWebServerDataResponse responseWithText:body];
-        responseObj.statusCode = status;
-        [headers enumerateKeysAndObjectsUsingBlock:^(id key, id value, BOOL* stop) {
-            [responseObj setValue:value forAdditionalHeader:key];
+    for (NSString* method in @[@"GET", @"POST", @"OPTIONS"]) {
+        [webServer addDefaultHandlerForMethod:method
+                                 requestClass:[GCDWebServerDataRequest class]
+                                  processBlock:^GCDWebServerResponse*(GCDWebServerRequest* request) {
+            __strong __typeof__(weakSelf) strongSelf = weakSelf;
+            if (!strongSelf) return nil;
+
+            // Create and store wrapper
+            NSString* reqId = [[NSUUID UUID] UUIDString];
+            RequestWrapper* rw = [RequestWrapper new];
+            rw.semaphore = dispatch_semaphore_create(0);
+            rw.response = nil;
+            strongSelf->pendingRequests[reqId] = rw;
+
+            // Extract request details
+            NSString* httpMethod = request.method;
+            NSString* path = request.URL.path ?: @"";
+            NSString* query = request.URL.query ?: @"";
+            NSString* remoteAddr = @"";
+            NSData* addrData = [request respondsToSelector:@selector(remoteAddressData)] ? request.remoteAddressData : nil;
+            if (addrData) {
+                struct sockaddr *addr = (struct sockaddr*)addrData.bytes;
+                char buffer[INET6_ADDRSTRLEN];
+                const void* src = (addr->sa_family == AF_INET) ?
+                    (void*)&((struct sockaddr_in*)addr)->sin_addr :
+                    (void*)&((struct sockaddr_in6*)addr)->sin6_addr;
+                if (inet_ntop(addr->sa_family, src, buffer, sizeof(buffer))) {
+                    remoteAddr = [NSString stringWithUTF8String:buffer];
+                }
+            }
+            NSDictionary* headers = [request respondsToSelector:@selector(headers)] ? request.headers : @{};
+            NSString* bodyString = @"";
+            if (([httpMethod isEqualToString:@"POST"] || [httpMethod isEqualToString:@"PUT"]) &&
+                            [request isKindOfClass:[GCDWebServerDataRequest class]]) {
+                            GCDWebServerDataRequest* dataReq = (GCDWebServerDataRequest*)request;
+                            bodyString = [[NSString alloc] initWithData:dataReq.data encoding:NSUTF8StringEncoding] ?: @"";
+                        }
+
+            // Send request info to JS
+            NSDictionary* jsReq = @{ @"requestId": reqId,
+                                     @"method": httpMethod,
+                                     @"path": path,
+                                     @"query": query,
+                                     @"headers": headers,
+                                     @"body": bodyString,
+                                     @"remoteAddress": remoteAddr };
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsReq];
+            [pluginResult setKeepCallbackAsBool:YES];
+            [strongSelf.commandDelegate sendPluginResult:pluginResult callbackId:strongSelf->requestCallbackId];
+
+            // Wait for JS response or timeout
+            dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, 30 * NSEC_PER_SEC);
+            long wait = dispatch_semaphore_wait(rw.semaphore, timeout);
+
+            NSDictionary* respDict = rw.response;
+            GCDWebServerDataResponse* respObj;
+            if (wait != 0 || !respDict) {
+                respObj = [GCDWebServerDataResponse responseWithText:@"Timeout waiting for response"];
+                respObj.statusCode = 500;
+            } else {
+                NSInteger status = [respDict[@"status"] integerValue];
+                NSString* respBody = respDict[@"body"] ?: @"";
+                respObj = [GCDWebServerDataResponse responseWithText:respBody];
+                respObj.statusCode = status;
+                NSDictionary* respHeaders = respDict[@"headers"] ?: @{};
+                [respHeaders enumerateKeysAndObjectsUsingBlock:^(id key, id value, BOOL *stop) {
+                    [respObj setValue:value forAdditionalHeader:key];
+                }];
+            }
+
+            // Clean up
+            [strongSelf->pendingRequests removeObjectForKey:reqId];
+            return respObj;
         }];
-        return responseObj;
-    }];
-    [self->webServer startWithOptions:@{
-      GCDWebServerOption_Port: @(port),
-      GCDWebServerOption_BindToLocalhost: @NO,
-      GCDWebServerOption_AutomaticallySuspendInBackground: @NO
-    } error:nil];
+    }
+
+    [webServer startWithOptions:@{ GCDWebServerOption_Port: @(port),
+                                  GCDWebServerOption_BindToLocalhost: @NO,
+                                  GCDWebServerOption_AutomaticallySuspendInBackground: @NO } error:nil];
 
     NSString* ip = [self getWiFiAddress];
     NSString* resultString = [NSString stringWithFormat:@"%@:%ld", ip, (long)port];
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:resultString];
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:resultString];
+    [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
 }
 
-- (NSString *)getWiFiAddress {
+- (void)stop:(CDVInvokedUrlCommand*)command {
+    [webServer stop];
+    [pendingRequests removeAllObjects];
+    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Server stopped"];
+    [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+}
+
+- (void)onRequest:(CDVInvokedUrlCommand*)command {
+    requestCallbackId = command.callbackId;
+    CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT];
+    [res setKeepCallbackAsBool:YES];
+    [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+}
+
+- (void)sendResponse:(CDVInvokedUrlCommand*)command {
+    NSString* reqId = command.arguments[0];
+    NSDictionary* respDict = command.arguments[1];
+    RequestWrapper* rw = pendingRequests[reqId];
+    if (rw) {
+        rw.response = respDict;
+        dispatch_semaphore_signal(rw.semaphore);
+        CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Response sent"];
+        [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
+    } else {
+        CDVPluginResult* err = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid requestId"];
+        [self.commandDelegate sendPluginResult:err callbackId:command.callbackId];
+    }
+}
+
+- (NSString*)getWiFiAddress {
     NSString *address = @"127.0.0.1";
     struct ifaddrs *interfaces = NULL;
     struct ifaddrs *temp_addr = NULL;
-    int success = getifaddrs(&interfaces);
-    if (success == 0) {
+    if (getifaddrs(&interfaces) == 0) {
         temp_addr = interfaces;
         while (temp_addr != NULL) {
             if (temp_addr->ifa_addr->sa_family == AF_INET) {
@@ -86,39 +158,5 @@
     return address;
 }
 
-- (void)stop:(CDVInvokedUrlCommand*)command {
-    if (webServer) {
-        [webServer stop];
-        [pendingRequests removeAllObjects];
-        CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Server stopped"];
-        [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-    } else {
-        CDVPluginResult* err = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Server not running"];
-        [self.commandDelegate sendPluginResult:err callbackId:command.callbackId];
-    }
-}
-
-- (void)onRequest:(CDVInvokedUrlCommand*)command {
-    requestCallbackId = command.callbackId;
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT];
-    [pluginResult setKeepCallbackAsBool:YES];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:requestCallbackId];
-}
-
-- (void)sendResponse:(CDVInvokedUrlCommand*)command {
-    NSString* reqId = command.arguments[0];
-    NSDictionary* resp = command.arguments[1];
-    NSMutableDictionary* wrapper = pendingRequests[reqId];
-    if (wrapper) {
-        wrapper[@"response"] = resp;
-        dispatch_semaphore_signal(wrapper[@"semaphore"]);
-        [pendingRequests removeObjectForKey:reqId];
-        CDVPluginResult* res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Response sent"];
-        [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
-    } else {
-        CDVPluginResult* err = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid requestId"];
-        [self.commandDelegate sendPluginResult:err callbackId:command.callbackId];
-    }
-}
-
 @end
+
